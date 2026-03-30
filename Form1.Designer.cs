5da6c7e3c9d261a59252fa88bd9207dfa64e3f34
Ipek Gozlek <Ipek Gozlek@IAS-IGOZLEK>
2026-03-30 20:39:00 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Araç_Kullanimlari2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Bursa", "Ankara", "Balıkesir", "İzmir", "Malatya", "İstanbul" });
+            comboBox1.Location = new Point(37, 57);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(139, 32);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(61, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 24);
+            label1.TabIndex = 1;
+            label1.Text = "Şehirler";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(330, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Şehir:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(411, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 32);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(397, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Şehir ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "öğretmen", "doktor ", "mühendis" });
+            listBox1.Location = new Point(69, 172);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 100);
+            listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(99, 301);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 6;
+            button2.Text = "ekle";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 192);
+            ClientSize = new Size(1100, 540);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Font = new Font("Corbel", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Araç Kullanımları";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace Araç_Kullanimlari2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Yazar");
+        }
+    }
+}
